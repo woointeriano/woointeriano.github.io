8074e3a59f89bfd346575b815f1e22d3afe28956
woozzie <chriskfwoo@gmail.com>
2016-06-04 17:20:15 -0400

    projet slider fix

--- js/script.js
+++ js/script.js
@@ -7,11 +7,20 @@ jQuery(document).ready(function(){
         if ( $fc.hasClass("up") ){
             $fc.stop().animate({height:0},400);
             $fc.removeClass("up");
+            document.getElementById("arrowicon").className = "glyphicon glyphicon-menu-up";
+            //document.getElementById("arrowicon").className += " glyphicon-menu-up";
+
+
         }
         else {
             $fc.stop().animate({height:$fheight},400);
             $fc.addClass("up");
+            document.getElementById("arrowicon").className = "glyphicon glyphicon-menu-down";
+          //  document.getElementById("arrowicon").className += " glyphicon-menu-down";
         }
+
+
+
         return false;
     });
 });
