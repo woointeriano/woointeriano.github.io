69fb7e6fecdeaf2e00aeadcb4adebc543e3d9b6d
Johnny Interiano <benjonant@msn.com>
2016-06-04 19:45:41 -0400

    footer fix

--- js/script.js
+++ js/script.js
@@ -24,3 +24,17 @@ jQuery(document).ready(function(){
         return false;
     });
 });
+
+
+
+
+//for bootstrap card projects
+
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+
+})(jQuery);
